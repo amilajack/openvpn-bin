9a66883d97e5563e3ab912579b1bfdb1d71436ad
Amila Welihinda <amilajack@gmail.com>
2018-09-22 14:21:08 -0700

    conform code style

--- index.js
+++ index.js
@@ -7,117 +7,130 @@ const getPort = Promise.promisify(require('get-port'));
 const fs = Promise.promisifyAll(require('fs'));
 
 fs.existsAsync = function(path) {
-    return fs.openAsync(path, "r").then(function(stats) {
-        return true
-    }).catch(function(stats) {
-        return false
+  return fs
+    .openAsync(path, 'r')
+    .then(function(stats) {
+      return true;
     })
-}
+    .catch(function(stats) {
+      return false;
+    });
+};
 
 export const initialize = function(openvpnpath, args) {
-    return initialize(openvpnpath, args);
+  return initialize(openvpnpath, args);
 };
 
 export const shutdown = function() {
-    return shutdown();
+  return shutdown();
 };
 
 function shutdown() {
-    return new Promise(function(resolve, reject) {
-        new Promise(function(resolve, reject) {
-
-        })
-    });
+  return new Promise(function(resolve, reject) {
+    new Promise(function(resolve, reject) {});
+  });
 }
 
 function initialize(openvpnpath, args) {
-    return new Promise(function(resolve, reject) {
-        fs.existsAsync(args.config)
-            .then(function(exists) {
-                if (!exists)
-                    return console.info('OpenVpn Config file not found, defaulting to "config.ovpn"');
-            })
-            .then(function() {
-                if (!args.port) {
-                    return new Promise(function(resolve, reject) {
-                        getPort()
-                            .then(function(port) {
-                                args.port = port;
-                                resolve(args);
-                            }).catch(reject)
-                    });
-                } else {
-                    return args;
-                }
-            })
-            .then(getSetArgs)
-            .then(function(setargs) {
-                return child_process.execFileSync(openvpnpath,setargs);
-            })
-            .then(function() {
-                resolve({
-                    port: args.port,
-                    host: args.host
-                });
-            }).catch(reject);
-    });
+  return new Promise(function(resolve, reject) {
+    fs.existsAsync(args.config)
+      .then(function(exists) {
+        if (!exists)
+          return console.info(
+            'OpenVpn Config file not found, defaulting to "config.ovpn"'
+          );
+      })
+      .then(function() {
+        if (!args.port) {
+          return new Promise(function(resolve, reject) {
+            getPort()
+              .then(function(port) {
+                args.port = port;
+                resolve(args);
+              })
+              .catch(reject);
+          });
+        } else {
+          return args;
+        }
+      })
+      .then(getSetArgs)
+      .then(function(setargs) {
+        return child_process.execFileSync(openvpnpath, setargs);
+      })
+      .then(function() {
+        resolve({
+          port: args.port,
+          host: args.host
+        });
+      })
+      .catch(reject);
+  });
 }
 
 function getSetArgs(args) {
-    var newargs;
-    args = _.defaults(args, {
-        host: '127.0.0.1',
-        port: 1337, //port should *always* be set at this point but we will defualt it anyway to 1337 just incase.
-        scriptSecurity: 2,
-        config: 'config.ovpn',
-        cwd: process.cwd(),
-        up: false,
-        down: false
-    });
+  var newargs;
+  args = _.defaults(args, {
+    host: '127.0.0.1',
+    port: 1337, //port should *always* be set at this point but we will defualt it anyway to 1337 just incase.
+    scriptSecurity: 2,
+    config: 'config.ovpn',
+    cwd: process.cwd(),
+    up: false,
+    down: false
+  });
 
-    switch (process.platform) {
-        case 'win32':
-            return [
-                '--management', args.host, args.port,
-                '--config', args.config,
-                '--script-security', args.scriptSecurity,
-                '--management-query-passwords',
-                '--management-hold',
-                '--register-dns'
-            ];
-            break;
-        case 'darwin':
-            var arg = [
-                '--management', args.host, args.port,
-                '--config', args.config,
-                '--script-security', args.scriptSecurity,
-                '--management-query-passwords',
-                '--management-hold',
-                '--daemon'
-            ];
-            if (args.up)
-                arg.push('--up', args.up);
-            if (args.down)
-                arg.push('--down', args.down);
-            return arg;
-        case 'linux':
-            var arg = [
-                '--management', args.host, args.port,
-                '--config', args.config,
-                '--script-security', args.scriptSecurity,
-                '--cd', args.cwd,
-                '--management-query-passwords',
-                '--management-hold',
-                '--daemon',
-                '--dev',
-                'tun0'
-            ];
-            if (args.up)
-                arg.push('--up', args.up);
-            if (args.down)
-                arg.push('--down', args.down);
-            return arg;
-            break;
-    }
-
+  switch (process.platform) {
+    case 'win32':
+      return [
+        '--management',
+        args.host,
+        args.port,
+        '--config',
+        args.config,
+        '--script-security',
+        args.scriptSecurity,
+        '--management-query-passwords',
+        '--management-hold',
+        '--register-dns'
+      ];
+      break;
+    case 'darwin':
+      var arg = [
+        '--management',
+        args.host,
+        args.port,
+        '--config',
+        args.config,
+        '--script-security',
+        args.scriptSecurity,
+        '--management-query-passwords',
+        '--management-hold',
+        '--daemon'
+      ];
+      if (args.up) arg.push('--up', args.up);
+      if (args.down) arg.push('--down', args.down);
+      return arg;
+    case 'linux':
+      var arg = [
+        '--management',
+        args.host,
+        args.port,
+        '--config',
+        args.config,
+        '--script-security',
+        args.scriptSecurity,
+        '--cd',
+        args.cwd,
+        '--management-query-passwords',
+        '--management-hold',
+        '--daemon',
+        '--dev',
+        'tun0'
+      ];
+      if (args.up) arg.push('--up', args.up);
+      if (args.down) arg.push('--down', args.down);
+      return arg;
+      break;
+  }
 }
--- test.js
+++ test.js
@@ -5,36 +5,36 @@ import path from 'path';
 const argv = argvFactory(process.argv.slice(2));
 
 if (argv.init) {
-    var openvpnpath = path.normalize(getOpenVPNPath()), //path of openvpn exsecutable
-        args = {
-            host: '127.0.0.1',
-            port: 1337,
-            scriptSecurity: 2,
-            config: 'config.ovpn'
-        };
-    console.log(openvpnbin.initialize(openvpnpath, args));
+  var openvpnpath = path.normalize(getOpenVPNPath()), //path of openvpn exsecutable
+    args = {
+      host: '127.0.0.1',
+      port: 1337,
+      scriptSecurity: 2,
+      config: 'config.ovpn'
+    };
+  console.log(openvpnbin.initialize(openvpnpath, args));
 }
 
 if (argv.shutdown) {
-    var openvpnpath = path.normalize(getOpenVPNPath()),
-        args = {
-            host: '127.0.0.1',
-            port: 1337,
-            scriptSecurity: 2,
-            config: 'config.ovpn'
-        };
-    openvpnbin.initialize(openvpnpath, args);
+  var openvpnpath = path.normalize(getOpenVPNPath()),
+    args = {
+      host: '127.0.0.1',
+      port: 1337,
+      scriptSecurity: 2,
+      config: 'config.ovpn'
+    };
+  openvpnbin.initialize(openvpnpath, args);
 }
 
 function getOpenVPNPath() {
-    switch (process.platform) {
-        case 'win32':
-            return '../bin/openvpn.exe'
-            break;
-        case 'darwin':
-            return '/usr/local/opt/openvpn/sbin/openvpn';
-        case 'linux':
-            return '/usr/local/opt/openvpn/sbin/openvpn';
-            break;
-    }
+  switch (process.platform) {
+    case 'win32':
+      return '../bin/openvpn.exe';
+      break;
+    case 'darwin':
+      return '/usr/local/opt/openvpn/sbin/openvpn';
+    case 'linux':
+      return '/usr/local/opt/openvpn/sbin/openvpn';
+      break;
+  }
 }

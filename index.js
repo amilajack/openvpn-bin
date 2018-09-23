a26b905429531061a65b073d09888a0de942e6e7
Amila Welihinda <amilajack@gmail.com>
2018-09-22 19:39:54 -0700

    add lint script as test script

--- index.js
+++ index.js
@@ -1,72 +1,24 @@
 import Promise from 'bluebird';
-import _ from 'lodash';
-import path from 'path';
-import child_process from 'child_process';
+import childProcess from 'child_process';
 
 const getPort = Promise.promisify(require('get-port'));
 const fs = Promise.promisifyAll(require('fs'));
 
-fs.existsAsync = function (path) {
-  return fs
-    .openAsync(path, 'r')
-    .then(stats => true)
-    .catch(stats => false);
-};
+fs.existsAsync = _path => fs
+  .openAsync(_path, 'r')
+  .then(() => true)
+  .catch(() => false);
 
-export const initialize = function (openvpnpath, args) {
-  return initialize(openvpnpath, args);
-};
-
-export const shutdown = function () {
-  return shutdown();
-};
-
-function shutdown() {
-  return new Promise(((resolve, reject) => {
-    new Promise(((resolve, reject) => {}));
-  }));
-}
-
-function initialize(openvpnpath, args) {
-  return new Promise(((resolve, reject) => {
-    fs.existsAsync(args.config)
-      .then((exists) => {
-        if (!exists) {
-          return console.info(
-            'OpenVpn Config file not found, defaulting to "config.ovpn"'
-          );
-        }
-      })
-      .then(() => {
-        if (!args.port) {
-          return new Promise(((resolve, reject) => {
-            getPort()
-              .then((port) => {
-                args.port = port;
-                resolve(args);
-              })
-              .catch(reject);
-          }));
-        }
-        return args;
-      })
-      .then(getSetArgs)
-      .then(setargs => child_process.execFileSync(openvpnpath, setargs))
-      .then(() => {
-        resolve({
-          port: args.port,
-          host: args.host
-        });
-      })
-      .catch(reject);
-  }));
+export async function shutdown() {
+  return true;
 }
 
 function getSetArgs(args) {
-  let newargs;
-  args = _.defaults(args, {
+  const newArgs = Object.assign(args, {
     host: '127.0.0.1',
-    port: 1337, // port should *always* be set at this point but we will defualt it anyway to 1337 just incase.
+    // port should *always* be set at this point but we will
+    // defualt it anyway to 1337 just incase.
+    port: 1337,
     scriptSecurity: 2,
     config: 'config.ovpn',
     cwd: process.cwd(),
@@ -78,53 +30,76 @@ function getSetArgs(args) {
     case 'win32':
       return [
         '--management',
-        args.host,
-        args.port,
+        newArgs.host,
+        newArgs.port,
         '--config',
-        args.config,
+        newArgs.config,
         '--script-security',
-        args.scriptSecurity,
+        newArgs.scriptSecurity,
         '--management-query-passwords',
         '--management-hold',
         '--register-dns'
       ];
-      break;
-    case 'darwin':
-      var arg = [
+    case 'darwin': {
+      const arg = [
         '--management',
-        args.host,
-        args.port,
+        newArgs.host,
+        newArgs.port,
         '--config',
-        args.config,
+        newArgs.config,
         '--script-security',
-        args.scriptSecurity,
+        newArgs.scriptSecurity,
         '--management-query-passwords',
         '--management-hold',
         '--daemon'
       ];
-      if (args.up) arg.push('--up', args.up);
-      if (args.down) arg.push('--down', args.down);
+      if (newArgs.up) arg.push('--up', newArgs.up);
+      if (newArgs.down) arg.push('--down', newArgs.down);
       return arg;
-    case 'linux':
-      var arg = [
+    }
+    case 'linux': {
+      const arg = [
         '--management',
-        args.host,
-        args.port,
+        newArgs.host,
+        newArgs.port,
         '--config',
-        args.config,
+        newArgs.config,
         '--script-security',
-        args.scriptSecurity,
+        newArgs.scriptSecurity,
         '--cd',
-        args.cwd,
+        newArgs.cwd,
         '--management-query-passwords',
         '--management-hold',
         '--daemon',
         '--dev',
         'tun0'
       ];
-      if (args.up) arg.push('--up', args.up);
-      if (args.down) arg.push('--down', args.down);
+      if (newArgs.up) arg.push('--up', newArgs.up);
+      if (newArgs.down) arg.push('--down', newArgs.down);
       return arg;
-      break;
+    }
+    default:
+      throw new Error('Unsupported platform');
   }
+}
+
+export async function initialize(openvpnpath, args) {
+  const exists = fs.existsAsync(args.config);
+  if (!exists) {
+    console.info(
+      'OpenVpn Config file not found, defaulting to "config.ovpn"'
+    );
+  }
+
+  const newArgs = getSetArgs(!args.port ? {
+    ...args,
+    port: await getPort()
+  } : args);
+
+  childProcess.execFileSync(openvpnpath, newArgs);
+
+  return {
+    port: args.port,
+    host: args.host
+  };
 }
--- test.js
+++ test.js
@@ -1,40 +1,42 @@
 import argvFactory from 'minimist';
 import path from 'path';
-import openvpnbin from '..';
+import { initialize } from '.';
 
 const argv = argvFactory(process.argv.slice(2));
-
-if (argv.init) {
-  const openvpnpath = path.normalize(getOpenVPNPath()); // path of openvpn exsecutable
-  const args = {
-    host: '127.0.0.1',
-    port: 1337,
-    scriptSecurity: 2,
-    config: 'config.ovpn'
-  };
-  console.log(openvpnbin.initialize(openvpnpath, args));
-}
-
-if (argv.shutdown) {
-  const openvpnpath = path.normalize(getOpenVPNPath());
-  const args = {
-    host: '127.0.0.1',
-    port: 1337,
-    scriptSecurity: 2,
-    config: 'config.ovpn'
-  };
-  openvpnbin.initialize(openvpnpath, args);
-}
 
 function getOpenVPNPath() {
   switch (process.platform) {
     case 'win32':
       return '../bin/openvpn.exe';
-      break;
     case 'darwin':
       return '/usr/local/opt/openvpn/sbin/openvpn';
     case 'linux':
       return '/usr/local/opt/openvpn/sbin/openvpn';
-      break;
+    default:
+      throw new Error('Unsupported platform');
   }
 }
+
+(async () => {
+  if (argv.init) {
+    const openvpnpath = path.normalize(getOpenVPNPath()); // path of openvpn exsecutable
+    const args = {
+      host: '127.0.0.1',
+      port: 1337,
+      scriptSecurity: 2,
+      config: 'config.ovpn'
+    };
+    console.log(await initialize(openvpnpath, args));
+  }
+
+  if (argv.shutdown) {
+    const openvpnpath = path.normalize(getOpenVPNPath());
+    const args = {
+      host: '127.0.0.1',
+      port: 1337,
+      scriptSecurity: 2,
+      config: 'config.ovpn'
+    };
+    await initialize(openvpnpath, args);
+  }
+})();
